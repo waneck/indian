321b62ce6df39bd51fdf35edf81e2be3b3fd0e55
Cauê Waneck <waneck@gmail.com>
2014-06-11 18:16:43 -0300

    sync

--- src/indian/buf/Ptr.hx
+++ src/indian/buf/Ptr.hx
@@ -1,17 +1,33 @@
 package indian.buf;
 
 /**
-	Represents a Pointer to an unmanaged memory chunk or to the stack.
-
-	Shouldn't point to a managed memory location - thus avoiding the problems with moving GC's, pinning and managed memory layout
+	A Ptr represents a pointer to a natively-allocated memory chunk.
 **/
-class Ptr<T>
+abstract Ptr<T>(PtrData<T>)
 {
-#if cpp
-	var value:cpp.Pointer<T>;
-#elseif cs
-	var value:cs.system.IntPtr;
-#else
-	var value:haxe.Int64;
-#end
+	inline public function new(pointer)
+	{
+		this = pointer;
+	}
+
+	inline public function descriptor():MemChunk
+	{
+	}
+
+	inline public function eq(to:Ptr<T>):Bool
+	{
+
+	}
+
+	@:deprecated("Do not use the equality operator with pointers as it's not reliable. Use `eq' instead")
+	@:op(A == B) inline public function equals(to:Ptr<T>):Bool
+	{
+		return eq(to);
+	}
+
+	@:deprecated("Do not use the equality operator with pointers as it's not reliable. Use `eq' instead")
+	@:op(A != B) inline public function notEquals(to:Ptr<T>):Bool
+	{
+		return !eq(to);
+	}
 }
---
+++ src/indian/buf/PtrData.hx
@@ -0,0 +1,21 @@
+package indian.buf.PtrData;
+
+typedef PtrData<T> =
+#if INDIAN_EMULATE_BUFFER
+	haxe.io.BytesData;
+#elseif cpp
+	cpp.Pointer<Void>;
+#elseif cs
+	cs.system.IntPtr;
+#elseif (js && nodejs)
+	indian._internal.nodejs.Buffer;
+#elseif js
+	js.html.ArrayBuffer;
+#elseif java
+	java.nio.ByteBuffer;
+// #elseif python
+#elseif neko
+	haxe.io.BytesData;
+#else
+	haxe.io.BytesData;
+#end
