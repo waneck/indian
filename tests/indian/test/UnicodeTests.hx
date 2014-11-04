f2b31ca975ba0dff1a91f3d3ce3a4e547b469439
Cauê Waneck <waneck@gmail.com>
2014-11-04 04:20:24 -0200

    basic unicode support

--- tests/indian/test/UnicodeTests.hx
+++ tests/indian/test/UnicodeTests.hx
@@ -17,7 +17,6 @@ import utest.Assert;
 			'Just a normal ASCII string here!',
 			'We only\n\rtest\r\n ASCII strings here. Promise!',
 			'',
-			// 'Olá, Mundo',
 		];
 		for (s in strings)
 		{
@@ -164,56 +163,52 @@ import utest.Assert;
 		}
 	}
 
-	// public function test_string()
-	// {
-	// 	var encodings = [Utf8.cur, Utf16.cur, Utf32.cur];
-	// 	var strings = [
-	// 		'Hello, World',
-	// 		'Olá, Mundo',
-	// 		'¡Hola mundo!',
-	// 		'привет мир',
-	// 		'STARGΛ̊TE SG-1, a = v̇ = r̈, a⃑ ⊥ b⃑', //combining characters
-	// 		'Σὲ γνωρίζω ἀπὸ τὴν κόψη', //greek polytonic
-	// 		'Οὐχὶ ταὐτὰ παρίσταταί μοι γιγνώσ\nκειν, ὦ ἄνδρες ᾿Αθηναῖοι', //greek
-	// 		'გთხოვთ ახლავე გაიაროთ რეგისტრა\nცია Unicode-ის მეათე საერთაშორისო', //georgian
-	// 		'Зарегистрируйтесь сейчас \nна Десятую Международную Конференцию по', //russian
-	// 		'๏ แผ่นดินฮั่นเสื่อมโทรมแสนสังเวช  พระปกเกศกองบู๊กู้ขึ้นใหม่', //thai - 2 columns
-	// 		'ሰማይ አይታረስ ንጉሥ አይከሰስ።', //ethiopian
-	// 		'ᚻᛖ ᚳᚹᚫᚦ ᚦᚫᛏ ᚻᛖ ᛒᚢᛞᛖ ᚩᚾ \nᚦᚫᛗ ᛚᚪᚾᛞᛖ ᚾᚩᚱᚦᚹᛖᚪᚱᛞᚢᛗ ᚹᛁᚦ ᚦᚪ ᚹᛖᛥᚫ', //runes
-	// 		'⡌⠁⠧⠑ ⠼⠁⠒  ⡍⠜⠇⠑⠹⠰⠎ ⡣⠕⠌', //braille
-	// 		'ABCDEFGHIJKLMNOPQRSTUVWXYZ /012345\n6789', //compact font
-	// 		'∀∂∈ℝ∧∪≡∞ ↑↗↨↻⇣ ┐┼╔╘░►☺♀ ﬁ�⑀₂ἠḂӥẄɐː\n⍎אԱა', //more compact
-	// 		'Hello world, Καλημέρα κ\nόσμε, コンニチハ', //more hello worlds
-	// 	];
-	// 	for (s in strings)
-	// 	{
-	// 		for (e1 in encodings)
-	// 		{
-	// 			// for (e2 in encodings)
-	// 			{
-	// 				var l1 = e1.neededLength(s);
-	// 						// l2 = e2.neededLength(s);
-	// 				autofree(
-	// 					b1e1 = $alloc(l1),
-	// 					// b1e2 = $alloc(l2),
-	// 					b2e1 = $alloc(Std.int(l1/2)),
-	// 					// b2e2 = $alloc(Std.int(l2/2)),
-	// 					b3e1 = $alloc(l1*2),
-	// 					// b3e2 = $alloc(l2*2),
-	// 				{
-	// 					// test back and forth
-	// 					e1.convertFromString(s,b1e1,l1);
-	// 					var s2 = e1.convertToString(b1e1,l1);
-	// 					Assert.equals(s,s2);
-	// 					trace(e1,l1);
-	// 					trace(s.length,s2.length);
-	// 					trace(s,s2,s == s2);
-	// 					// Assert.equals(l1,r);
-	// 				});
-	// 			}
-	// 		}
-	// 	}
-	// }
+	public function test_string()
+	{
+		var encodings = [Utf8.cur, Utf16.cur, Utf32.cur];
+		var strings = [
+			'Hello, World',
+			'Olá, Mundo',
+			'¡Hola mundo!',
+			'привет мир',
+			'STARGΛ̊TE SG-1, a = v̇ = r̈, a⃑ ⊥ b⃑', //combining characters
+			'Σὲ γνωρίζω ἀπὸ τὴν κόψη', //greek polytonic
+			'Οὐχὶ ταὐτὰ παρίσταταί μοι γιγνώσ\nκειν, ὦ ἄνδρες ᾿Αθηναῖοι', //greek
+			'გთხოვთ ახლავე გაიაროთ რეგისტრა\nცია Unicode-ის მეათე საერთაშორისო', //georgian
+			'Зарегистрируйтесь сейчас \nна Десятую Международную Конференцию по', //russian
+			'๏ แผ่นดินฮั่นเสื่อมโทรมแสนสังเวช  พระปกเกศกองบู๊กู้ขึ้นใหม่', //thai - 2 columns
+			'ሰማይ አይታረስ ንጉሥ አይከሰስ።', //ethiopian
+			'ᚻᛖ ᚳᚹᚫᚦ ᚦᚫᛏ ᚻᛖ ᛒᚢᛞᛖ ᚩᚾ \nᚦᚫᛗ ᛚᚪᚾᛞᛖ ᚾᚩᚱᚦᚹᛖᚪᚱᛞᚢᛗ ᚹᛁᚦ ᚦᚪ ᚹᛖᛥᚫ', //runes
+			'⡌⠁⠧⠑ ⠼⠁⠒  ⡍⠜⠇⠑⠹⠰⠎ ⡣⠕⠌', //braille
+			'ABCDEFGHIJKLMNOPQRSTUVWXYZ /012345\n6789', //compact font
+			'∀∂∈ℝ∧∪≡∞ ↑↗↨↻⇣ ┐┼╔╘░►☺♀ ﬁ�⑀₂ἠḂӥẄɐː\n⍎אԱა', //more compact
+			'Hello world, Καλημέρα κ\nόσμε, コンニチハ', //more hello worlds
+			''
+		];
+		for (s in strings)
+		{
+			for (e1 in encodings)
+			{
+				for (e2 in encodings)
+				{
+					var l1 = e1.neededLength(s,true),
+							l2 = e2.neededLength(s,true);
+					autofree(
+						buf = $alloc(l1),
+					{
+						// test back and forth
+						e1.convertFromString(s,buf,l1,true);
+						var s2 = e1.convertToString(buf,l1,true);
+						Assert.equals(s,s2);
+						// trace(e1,l1);
+						// trace(s.length,s2.length);
+						// trace(s,s2,s == s2);
+						// Assert.equals(l1,r);
+					});
+				}
+			}
+		}
+	}
 
 	//- string -> encoding and back. with same obj, exact length, less length and much more length
 	//- encoding -> encoding and back. same obj, exact length, less length and much more length
