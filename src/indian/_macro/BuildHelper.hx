01c274bb501fb9a9ed95cb7c03cd6fe8623cea8f
Cauê Waneck <waneck@gmail.com>
2015-05-26 15:06:19 -0300

    Better name structs and pointers if possible

--- src/indian/_macro/BuildHelper.hx
+++ src/indian/_macro/BuildHelper.hx
@@ -1,4 +1,6 @@
 package indian._macro;
+import haxe.macro.Context;
+import haxe.macro.Context.*;
 import indian._macro.helpers.*;
 using StringTools;
 
@@ -14,5 +16,28 @@ class BuildHelper
 	public static function shortType(pack:Array<String>,name:String)
 	{
 		return shortPack.encode(pack,name);
+	}
+
+	static var regex = ~/typedef[ \t\n]+([A-Za-z0-9_]+)/g;
+
+	public static function tryGetDeclaringTypedef():Null<{ pack:Array<String>, name:String }>
+	{
+		var r = getPosInfos(currentPos());
+		try
+		{
+			var f = sys.io.File.read(r.file);
+			f.seek(r.min, SeekBegin);
+			var data = f.readString(r.max - r.min);
+			f.close();
+			if (regex.match(data))
+			{
+				var pack = getLocalModule().split('.');
+				pack.pop();
+				return { pack:pack, name:regex.matched(1) };
+			}
+		}
+		catch(e:Dynamic) {}
+
+		return null;
 	}
 }
